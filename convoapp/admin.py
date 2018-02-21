d632586d08414a9daab1eda3ea52c9d33f6538f3
Charles <charlesdsmith25@gmail.com>
2018-02-20 22:17:24 -0500

    Update admin.py

--- convoapp/admin.py
+++ convoapp/admin.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from models import InputInfo
-#from kombu.transport.django import models as kombu_models
 
 admin.site.register(InputInfo)
-#admin.site.register(kombu_models.Message)
